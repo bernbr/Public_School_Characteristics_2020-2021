798757312599043f3670d3cc9813acda83b3c590
BMO777 <bomirera@gmail.com>
2023-04-19 04:39:07 -0500

    accomodate app.py to serve geojson file

--- app.py
+++ app.py
@@ -3,15 +3,13 @@
 from flask import Flask
 from flask_cors import CORS, cross_origin
 from pymongo import MongoClient
-from bson.json_util import dumps, loads
+from bson.json_util import dumps
 
 #################################################
 # Load Data
 #################################################
 #Import csv to mongo with: mongoimport --type csv -d PublicSchool2021 -c characteristics --headerline --drop Compiled_Public_School_Characteristics.csv
-# Connect to YOUR database here...
-# To keep this example simple, I'm just loading a data.json file 
-# But for your project, this is where the database connection should go
+
 mongo = MongoClient(port=27017)
 db = mongo['PublicSchool2021']
 characteristics = db['characteristics']
